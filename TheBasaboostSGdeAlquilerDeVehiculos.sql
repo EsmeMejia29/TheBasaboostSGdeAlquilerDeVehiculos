6bfe5a9078ff8cfb61667903b2d560761f479510
CheluwwDev <20245083@esen.edu.sv>
2025-07-05 06:14:44 -0600

    Update TheBasaboostSGdeAlquilerDeVehiculos.sql

--- TheBasaboostSGdeAlquilerDeVehiculos.sql
+++ TheBasaboostSGdeAlquilerDeVehiculos.sql
@@ -2,7 +2,7 @@ DROP DATABASE TheBasaboostSGdeAlquilerDeVehiculos
 CREATE DATABASE TheBasaboostSGdeAlquilerDeVehiculos;
 USE TheBasaboostSGdeAlquilerDeVehiculos;
 
------ CREACIÓN DE LA BASE DE DATOS -----
+----- CREACIÃ“N DE LA BASE DE DATOS -----
 
 CREATE TABLE DEPARTAMENTO(
 	id_departamento TINYINT PRIMARY KEY IDENTITY,
@@ -41,6 +41,55 @@ CREATE TABLE CLIENTE(
 	telefono VARCHAR(13),
 	fecha_registro DATE
 );
+
+CREATE TABLE ESTADO_RESERVA (
+    id_estado_r VARCHAR(12) PRIMARY KEY,
+    estado_v VARCHAR(40)
+);
+
+CREATE TABLE RESERVA (
+    id_reserva VARCHAR(15) PRIMARY KEY,
+    id_cliente VARCHAR(50),
+    matricula VARCHAR(50),
+    fecha_inicio_alquiler DATE,
+    fecha_fin_alquiler DATE,
+    id_estado_r VARCHAR(12),
+    kilometraje_estimado VARCHAR(30),
+    FOREIGN KEY (id_cliente) REFERENCES CLIENTE(id_cliente),
+    FOREIGN KEY (matricula) REFERENCES VEHICULO(matricula),
+    FOREIGN KEY (id_estado_r) REFERENCES ESTADO_RESERVA(id_estado_r)
+);
+
+CREATE TABLE METODO (
+    id_metodo_pago VARCHAR(10) PRIMARY KEY,
+    metodo_pago VARCHAR(30)
+);
+
+CREATE TABLE PAGO (
+    id_pago VARCHAR(10) PRIMARY KEY,
+    id_reserva VARCHAR(15),
+    monto DECIMAL(8, 2),
+    fecha_pago DATE,
+    id_metodo_pago VARCHAR(10),
+    FOREIGN KEY (id_reserva) REFERENCES RESERVA(id_reserva),
+    FOREIGN KEY (id_metodo_pago) REFERENCES METODO(id_metodo_pago)
+);
+
+CREATE TABLE SERVICIOS_ADICIONALES (
+    id_servicio_adicional VARCHAR(15) PRIMARY KEY,
+    nombre VARCHAR(35),
+    costo DECIMAL(7, 2),
+    descripcion VARCHAR(80)
+);
+
+CREATE TABLE SERVICIO_RESERVA (
+    id_servicio_reserva VARCHAR(10) PRIMARY KEY,
+    id_servicio_adicional VARCHAR(15),
+    id_reserva VARCHAR(15),
+    FOREIGN KEY (id_servicio_adicional) REFERENCES SERVICIOS_ADICIONALES(id_servicio_adicional),
+    FOREIGN KEY (id_reserva) REFERENCES RESERVA(id_reserva)
+);
+
 
 
 ALTER TABLE DEPARTAMENTO ADD FOREIGN KEY(id_empleado) REFERENCES PUESTO_EMPLEADO(id_empleado) ON DELETE CASCADE;
