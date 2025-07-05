6af2f4593002232aea0830a556e1d83b4398aeba
EsmeMejia29 <esmejia2908@gmail.com>
2025-07-05 12:56:15 -0600

    UPDATE formato a tablas recientes

--- TheBasaboostSGdeAlquilerDeVehiculos.sql
+++ TheBasaboostSGdeAlquilerDeVehiculos.sql
@@ -3,7 +3,7 @@ CREATE DATABASE TheBasaboostSGdeAlquilerDeVehiculos;
 USE TheBasaboostSGdeAlquilerDeVehiculos;
 
 ----- CREACIÓN DE LA BASE DE DATOS -----
-
+--- TABLAS:
 CREATE TABLE DEPARTAMENTO(
 	id_departamento TINYINT PRIMARY KEY IDENTITY,
 	nombre_departamento VARCHAR(35),
@@ -42,22 +42,25 @@ CREATE TABLE CLIENTE(
 	fecha_registro DATE
 );
 
-CREATE TABLE ESTADO_RESERVA (
-    id_estado_r VARCHAR(12) PRIMARY KEY,
-    estado_v VARCHAR(40)
-);
-
-CREATE TABLE RESERVA (
+CREATE TABLE RESERVA(
     id_reserva VARCHAR(15) PRIMARY KEY,
     id_cliente VARCHAR(50),
     matricula VARCHAR(50),
     fecha_inicio_alquiler DATE,
     fecha_fin_alquiler DATE,
     id_estado_r VARCHAR(12),
-    kilometraje_estimado VARCHAR(30),
-    FOREIGN KEY (id_cliente) REFERENCES CLIENTE(id_cliente),
-    FOREIGN KEY (matricula) REFERENCES VEHICULO(matricula),
-    FOREIGN KEY (id_estado_r) REFERENCES ESTADO_RESERVA(id_estado_r)
+    kilometraje_estimado VARCHAR(30)
+);
+
+CREATE TABLE ESTADO_RESERVA(
+    id_estado_r VARCHAR(12) PRIMARY KEY,
+    estado_v VARCHAR(40)
+);
+
+CREATE TABLE PAGO (
+    id_pago VARCHAR(10) PRIMARY KEY,
+    monto DECIMAL(8, 2),
+    fecha_pago DATE
 );
 
 CREATE TABLE METODO (
@@ -65,30 +68,32 @@ CREATE TABLE METODO (
     metodo_pago VARCHAR(30)
 );
 
-CREATE TABLE PAGO (
-    id_pago VARCHAR(10) PRIMARY KEY,
-    id_reserva VARCHAR(15),
-    monto DECIMAL(8, 2),
-    fecha_pago DATE,
-    id_metodo_pago VARCHAR(10),
-    FOREIGN KEY (id_reserva) REFERENCES RESERVA(id_reserva),
-    FOREIGN KEY (id_metodo_pago) REFERENCES METODO(id_metodo_pago)
+CREATE TABLE SERVICIO_RESERVA(
+    id_servicio_reserva VARCHAR(10) PRIMARY KEY
 );
 
-CREATE TABLE SERVICIOS_ADICIONALES (
+CREATE TABLE SERVICIOS_ADICIONALES(
     id_servicio_adicional VARCHAR(15) PRIMARY KEY,
     nombre VARCHAR(35),
     costo DECIMAL(7, 2),
     descripcion VARCHAR(80)
 );
 
-CREATE TABLE SERVICIO_RESERVA (
-    id_servicio_reserva VARCHAR(10) PRIMARY KEY,
-    id_servicio_adicional VARCHAR(15),
-    id_reserva VARCHAR(15),
-    FOREIGN KEY (id_servicio_adicional) REFERENCES SERVICIOS_ADICIONALES(id_servicio_adicional),
-    FOREIGN KEY (id_reserva) REFERENCES RESERVA(id_reserva)
-);
+
+-- FOREIGN KEYS:
+ALTER TABLE DEPARTAMENTO ADD FOREIGN KEY(id_empleado) REFERENCES PUESTO_EMPLEADO(id_empleado) ON DELETE CASCADE;
+ALTER TABLE EMPLEADO ADD FOREIGN KEY(id_puesto) REFERENCES PUESTO_EMPLEADO(id_puesto) ON DELETE CASCADE;
+ALTER TABLE MANTENIMIENTO ADD FOREIGN KEY(id_empleado) REFERENCES EMPLEADO(id_empleado) ON DELETE CASCADE;
+--- Falta el FK de matricula en la tabla de Mantenimiento
+ALTER TABLE RESERVA ADD FOREIGN KEY (id_cliente) REFERENCES CLIENTE(id_cliente) ON DELETE CASCADE;
+------ Descomentar cuando esté la tabla de Vehículo: ALTER TABLE RESERVA ADD FOREIGN KEY (matricula) REFERENCES VEHICULO(matricula)
+ALTER TABLE RESERVA ADD FOREIGN KEY (id_estado_r) REFERENCES ESTADO_RESERVA(id_estado_r) ON DELETE CASCADE;
+ALTER TABLE PAGO ADD FOREIGN KEY (id_reserva) REFERENCES RESERVA(id_reserva) ON DELETE CASCADE;
+ALTER TABLE PAGO ADD FOREIGN KEY (id_metodo_pago) REFERENCES METODO(id_metodo_pago) ON DELETE CASCADE;
+ALTER TABLE SERVICIO_RESERVA ADD FOREIGN KEY (id_servicio_adicional) REFERENCES SERVICIOS_ADICIONALES(id_servicio_adicional) ON DELETE CASCADE;
+ALTER TABLE SERVICIO_RESERVA ADD FOREIGN KEY (id_reserva) REFERENCES RESERVA(id_reserva) ON DELETE CASCADE;
+
+
 
 --  Intersección Empleado-Departamento
 CREATE TABLE EMPLEADO_DEPARTAMENTO (
@@ -99,7 +104,6 @@ CREATE TABLE EMPLEADO_DEPARTAMENTO (
     FOREIGN KEY (id_departamento) REFERENCES DEPARTAMENTO(id_departamento)
 );
 
-
 -- Intersección Reserva-Servicios Adicionales
 CREATE TABLE SERVICIO_RESERVA (
     id_reserva VARCHAR(15) NOT NULL,
@@ -109,9 +113,3 @@ CREATE TABLE SERVICIO_RESERVA (
     FOREIGN KEY (id_servicio_adicional) REFERENCES SERVICIOS_ADICIONALES(id_servicio_adicional)
 );
 
-
-
-ALTER TABLE DEPARTAMENTO ADD FOREIGN KEY(id_empleado) REFERENCES PUESTO_EMPLEADO(id_empleado) ON DELETE CASCADE;
-ALTER TABLE EMPLEADO ADD FOREIGN KEY(id_puesto) REFERENCES PUESTO_EMPLEADO(id_puesto) ON DELETE CASCADE;
-ALTER TABLE MANTENIMIENTO ADD FOREIGN KEY(id_empleado) REFERENCES EMPLEADO(id_empleado) ON DELETE CASCADE;
---- Falta el FK de matricula en la tabla de Mantenimiento
